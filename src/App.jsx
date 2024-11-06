a49d4d3c958d92ae2409b1dfcf78b39c468f2f87
alireza013013 <alireza.abdollahi19998@gmail.com>
2024-11-06 12:58:09 +0330

    back to original version

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home';
 
-const Login = () => {
-  return (
-    <div>
-      Test Page
-    </div>
-  )
-}
-
-const Dashboard = () => {
-  return (
-    <div>
-      a
-    </div>
-  )
-}
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          <Route index element={<Login />} />
-          <Route path="idglass" element={<Dashboard />} />
-          {/* <Route path="idglass" element={<Home />} />
-          <Route path=":idglass" element={<Home />} /> */}
-        </Route>
-        {/* <Route path="/home" element={<Dashboard />}>
-          <Route index path=":idglass" element={<Home />} />
-        </Route> */}
-        {/* <Route path="/home/:idglass" element={<Home />} /> */}
-        {/* <Route path="/home/:idglass">
-          <Route index element={<Home />} />
-        </Route> */}
+        <Route path="/:idglass" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
