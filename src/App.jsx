e91522aeea4eba4d10ce3bf0f61b3756f893ee7e
alireza013013 <alireza.abdollahi19998@gmail.com>
2024-11-06 12:08:07 +0330

    add home to url

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/:idglass">
+        <Route path="/home/:idglass">
           <Route index element={<Home />} />
         </Route>
       </Routes>
